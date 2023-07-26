fb69d6716514f51ac34936c49bfc607ed9751fd2
sahin <sahin@DESKTOP-1SCT98J>
2023-07-26 13:17:24 +0300

    Add project files.

---
+++ Casgem_RapidApi/Controllers/BookingController.cs
@@ -0,0 +1,32 @@
+﻿using Casgem_RapidApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Casgem_RapidApi.Controllers
+{
+    public class BookingController : Controller
+    {
+        public async Task <IActionResult> Index(string adult="1",string child="1",string checkinDate="2023-09-27", string checkoutDate="2023-09-28",string roomNumber="1",string cityID= "-553173")
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri($"https://booking-com.p.rapidapi.com/v2/hotels/search?order_by=popularity&adults_number={adult}&checkin_date={checkinDate}&filter_by_currency=USD&dest_id={cityID}&locale=en-gb&checkout_date={checkoutDate}&units=metric&room_number={roomNumber}&dest_type=city&include_adjacency=true&children_number={child}&page_number=0&children_ages=5%2C0&categories_filter_ids=class%3A%3A2%2Cclass%3A%3A4%2Cfree_cancellation%3A%3A1"),
+                Headers =
+    {
+        { "X-RapidAPI-Key", "630ce9cc86msh271c60cffe62d5ep1b514djsn0fe292593744" },
+        { "X-RapidAPI-Host", "booking-com.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<HotelListViewModel>(body);
+                return View(values.results.ToList());
+            }
+        }
+
+    }
+}
